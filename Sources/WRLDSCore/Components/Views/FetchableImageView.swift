432f062d71ae1f64d81a568f0019f20ef5b92348
Garrett <garrett@wrldsapp.com>
2021-01-04 12:47:54 -0500

    Update

--- Sources/WRLDSCore/Components/Views/FetchableImageView.swift
+++ Sources/WRLDSCore/Components/Views/FetchableImageView.swift
@@ -9,7 +9,7 @@
 import UIKit
 import SDWebImage
 
-public class FetchableImageView: UIImageView {
+open class FetchableImageView: UIImageView {
     
     public var placeholder: UIImage?
     
@@ -34,7 +34,7 @@ public class FetchableImageView: UIImageView {
         self.backgroundColor = .selectedCellBackground
     }
 
-    required init?(coder: NSCoder) {
+    required public init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
 }
